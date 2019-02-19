1e02e77cded8794dc071d199d6e323f2796eb91c
Abu-Hisham <abdulmoha786@gmail.com>
2019-02-19 13:17:31 -0800

    new changes

--- backend/classes/Enterprise.ts
+++ backend/classes/Enterprise.ts
@@ -6,7 +6,7 @@ import sql = require('mssql');
 
 export class Enterprise implements IKentaPayEnterprise {
     constructor() { }
-    validateInput(companyName: string, contactPersonsName: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string) {
+    validateRequest(companyName: string, contactPersonsName: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string) {
         const schema = Joi.object().keys({
             companyName: Joi.string().min(5).regex(/^[A-Z a-z]+$/).required().replace(/\s{2,}/g, ' '),
             contactPersonsName: Joi.string().min(3).regex(/^[A-Z a-z]+$/).max(100).required().replace(/\s{2,}/g, ' '),
@@ -19,9 +19,29 @@ export class Enterprise implements IKentaPayEnterprise {
         return result;
     }
 
-    corporate(companyName, contactPersonsName, companyUrl, emailAddress, phoneNumber, county): Promise<ActivityResponse> {
+    validateInput(name: string, company: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string, monthlyVolumes: string, averageTransactions: string) {
+        const schema = Joi.object().keys({
+            name: Joi.string().min(5).regex(/^[A-Z a-z]+$/).required().replace(/\s{2,}/g, ' '),
+            company: Joi.string().min(5).regex(/^[A-Z a-z]+$/).required().replace(/\s{2,}/g, ' '),
+            contactPersonsName: Joi.string().min(3).regex(/^[A-Z a-z]+$/).max(100).required().replace(/\s{2,}/g, ' '),
+            companyUrl: Joi.string().max(500).regex(/^(http|https):\/\/[^ "]+$/).required(),
+            emailAddress: Joi.string().max(100).email().required(),
+            phoneNumber: Joi.string().min(10).regex(/^[0-9]/).max(15).required().replace(/\s{1,}/g, ''),
+            county: Joi.string().required().min(3).regex(/^[A-Z a-z]+$/).replace(/\s{2,}/g, ' '),
+            monthlyVolumes: Joi.string().regex(/^[0-9]/).required().replace(/\s{1,}/g, ''),
+            averageTransactions: Joi.string().regex(/^\d+(\.\d{1,2})?$/).required().replace(/\s{1,}/g, '')
+        })
+        const result = Joi.validate({ name, company, companyUrl, emailAddress, phoneNumber, county, monthlyVolumes, averageTransactions }, schema);
+        return result;
+    }
+    corporateRequest(companyName, contactPersonsName, companyUrl, emailAddress, phoneNumber, county): Promise<ActivityResponse> {
+        return new Promise<ActivityResponse>(async(resolve,reject)=>{
+            let result = this.validateRequest(companyName, contactPersonsName, companyUrl, emailAddress, phoneNumber, county);
+        })
+    }
+    corporate(name: string, company: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string, monthlyVolumes: string, averageTransactions: string): Promise<ActivityResponse> {
         return new Promise<ActivityResponse>(async (resolve, reject) => {
-            let result = this.validateInput(companyName, contactPersonsName, companyUrl, emailAddress, phoneNumber, county);
+            let result = this.validateInput(name, company, companyUrl, emailAddress, phoneNumber, county, monthlyVolumes, averageTransactions);
             const isCoporate = 1;
             if (result.error == null) {
                 try {
@@ -36,22 +56,24 @@ export class Enterprise implements IKentaPayEnterprise {
                         });
                     } else {
                         try {
-                            let query1 = `SELECT * FROM [TBENTERPRISE] WHERE COMPANYNAME=@companyName OR COMPANYURL=@companyUrl`;
+                            let query1 = `SELECT * FROM [TBENTERPRISE] WHERE COMPANY=@company OR COMPANYURL=@companyUrl`;
                             let request = new sql.Request();
-                            request.input('companyName', result.value.companyName);
+                            request.input('company', result.value.company);
                             request.input('companyUrl', result.value.companyUrl);
                             let res = await request.query(query1);
                             if (res.recordsets[0].length === 0) {
                                 try {
-                                    let query2: string = `INSERT into [TBENTERPRISE] ([COMPANYNAME],[CONTACTPERSONSNAME],[COMPANYURL],[EMAILADDRESS],[MOBILENUMBER],[COUNTYID],[ISCORPORATE],[CREATEDAT]) 
-                                    VALUES(@companyName, @contactPersonsName, @companyUrl, @emailAddress, @phoneNumber,(SELECT RCID FROM TBCOUNTIES WHERE NAME=@county), @isCorporate,GETDATE());`;
+                                    let query2: string = `INSERT into [TBENTERPRISE] ([NAME],[COMPANY],[COMPANYURL],[EMAILADDRESS],[MOBILENUMBER],[COUNTYID],[MONTHLYVOLUMES],[AVERAGETRANSACTIONS],[ISCORPORATE],[CREATEDAT]) 
+                                    VALUES(@name, @company, @companyUrl, @emailAddress, @phoneNumber,(SELECT RCID FROM TBCOUNTIES WHERE NAME=@county),@monthlyVolumes, @averageTransactions, @isCorporate, GETDATE());`;
                                     let request = new sql.Request();
-                                    request.input('companyName', result.value.companyName);
-                                    request.input('contactPersonsName', result.value.contactPersonsName);
+                                    request.input('name', result.value.name);
+                                    request.input('company', result.value.company);
                                     request.input('companyUrl', result.value.companyUrl);
                                     request.input('emailAddress', result.value.emailAddress);
                                     request.input('phoneNumber', result.value.phoneNumber);
                                     request.input('county', result.value.county);
+                                    request.input('monthlyVolumes', result.value.monthlyVolumes);
+                                    request.input('averageTransactions', result.value.averageTransactions);
                                     request.input('isCorporate', sql.Bit, isCoporate);
                                     await request.query(query2);
                                     resolve({ type: 'success' });
@@ -90,9 +112,9 @@ export class Enterprise implements IKentaPayEnterprise {
         })
     }
 
-    merchant(companyName, contactPersonsName, companyUrl, emailAddress, phoneNumber, county): Promise<ActivityResponse> {
+    merchant(name: string, company: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string, monthlyVolumes: string, averageTransactions: string): Promise<ActivityResponse> {
         return new Promise<ActivityResponse>(async (resolve, reject) => {
-            let result = this.validateInput(companyName, contactPersonsName, companyUrl, emailAddress, phoneNumber, county);
+            let result = this.validateInput(name, company, companyUrl, emailAddress, phoneNumber, county, monthlyVolumes, averageTransactions);
             const isCoporate = 0;
             if (result.error == null) {
                 try {
@@ -107,22 +129,24 @@ export class Enterprise implements IKentaPayEnterprise {
                         });
                     } else {
                         try {
-                            let query1 = `SELECT * FROM [TBENTERPRISE] WHERE COMPANYNAME=@companyName OR COMPANYURL=@companyUrl`;
+                            let query1 = `SELECT * FROM [TBENTERPRISE] WHERE COMPANY=@company OR COMPANYURL=@companyUrl`;
                             let request = new sql.Request();
-                            request.input('companyName', result.value.companyName);
+                            request.input('company', result.value.company);
                             request.input('companyUrl', result.value.companyUrl);
                             let res = await request.query(query1);
                             if (res.recordsets[0].length === 0) {
                                 try {
-                                    let query2: string = `INSERT into [TBENTERPRISE] ([COMPANYNAME],[CONTACTPERSONSNAME],[COMPANYURL],[EMAILADDRESS],[MOBILENUMBER],[COUNTYID],[ISCORPORATE],[CREATEDAT]) 
-                                    VALUES(@companyName, @contactPersonsName, @companyUrl, @emailAddress, @phoneNumber,(SELECT RCID FROM TBCOUNTIES WHERE NAME=@county), @isCorporate,GETDATE());`;
+                                    let query2: string = `INSERT into [TBENTERPRISE] ([NAME],[COMPANY],[COMPANYURL],[EMAILADDRESS],[MOBILENUMBER],[COUNTYID],[MONTHLYVOLUMES],[AVERAGETRANSACTIONS],[ISCORPORATE],[CREATEDAT]) 
+                                    VALUES(@name, @company, @companyUrl, @emailAddress, @phoneNumber,(SELECT RCID FROM TBCOUNTIES WHERE NAME=@county),@monthlyVolumes, @averageTransactions, @isCorporate, GETDATE());`;
                                     let request = new sql.Request();
-                                    request.input('companyName', result.value.companyName);
-                                    request.input('contactPersonsName', result.value.contactPersonsName);
+                                    request.input('name', result.value.name);
+                                    request.input('company', result.value.company);
                                     request.input('companyUrl', result.value.companyUrl);
                                     request.input('emailAddress', result.value.emailAddress);
                                     request.input('phoneNumber', result.value.phoneNumber);
                                     request.input('county', result.value.county);
+                                    request.input('monthlyVolumes', result.value.monthlyVolumes);
+                                    request.input('averageTransactions', result.value.averageTransactions);
                                     request.input('isCorporate', sql.Bit, isCoporate);
                                     await request.query(query2);
                                     resolve({ type: 'success' });
--- backend/interfaces/IKentaPayEnterprise.ts
+++ backend/interfaces/IKentaPayEnterprise.ts
@@ -1,9 +1,70 @@
 import {ActivityResponse} from "../models/ActivityResponse";
 
+/**
+ *
+ * /
+ */
 export interface IKentaPayEnterprise {
 
-    corporate(companyName: string, contactPersonsName: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string):Promise<ActivityResponse>;
+    /**
+     * corporateRequest
+     *
+     * @param {string} companyName
+     * @param {string} contactPersonsName
+     * @param {string} companyUrl
+     * @param {string} emailAddress
+     * @param {string} phoneNumber
+     * @param {string} county
+     *
+     * @returns Promise<ActivityResponse>
+     */
+    corporateRequest(companyName: string, contactPersonsName: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string): Promise<ActivityResponse>;
 
-    merchant(companyName: string, contactPersonsName: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string):Promise<ActivityResponse>;
+    /**
+     * corporate
+     *
+     * @param {string} name
+     * @param {string} company
+     * @param {string} companyUrl
+     * @param {string} emailAddress
+     * @param {string} phoneNumber
+     * @param {string} county
+     * @param {string} monthlyVolumes
+     * @param {string} averageTransactions
+     *
+     * @returns Promise<ActivityResponse>
+     */
+    corporate(name: string, company: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string, monthlyVolumes: string, averageTransactions: string): Promise<ActivityResponse>;
+
+    /**
+     * merchantRequest
+     *
+     * @param {string} companyName
+     * @param {string} contactPersonsName
+     * @param {string} companyUrl
+     * @param {string} emailAddress
+     * @param {string} phoneNumber
+     * @param {string} county
+     *
+     * @returns Promise<ActivityResponse>
+     */
+    merchantRequest(companyName: string, contactPersonsName: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string): Promise<ActivityResponse>;
+
+    /**
+     * merchant
+     *
+     * @param {string} name
+     * @param {string} company
+     * @param {string} companyUrl
+     * @param {string} emailAddress
+     * @param {string} phoneNumber
+     * @param {string} county
+     * @param {string} monthlyVolumes
+     * @param {string} averageTransactions
+     *
+     * @returns Promise<ActivityResponse>
+     */
+    merchant(name: string, company: string, companyUrl: string, emailAddress: string, phoneNumber: string, county: string, monthlyVolumes: string, averageTransactions: string): Promise<ActivityResponse>;
+
 
 }
